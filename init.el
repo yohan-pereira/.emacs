f53b325244313ce34cfd54706005adf0b0f2ec6f
Yohan Pereira <yohan.pereira@gmail.com>
2017-10-10 12:37:34 +0530

    added go mode

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
     ("f782ed87369a7d568cee28d14922aa6d639f49dd676124d817dd82c8208985d0" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" default)))
  '(package-selected-packages
    (quote
-    (evil-magit magit exec-path-from-shell markdown-mode helm-ag robe enh-ruby-mode auto-complete evil-smartparens smartparens ag dirtree paredit pastels-on-dark-theme dracula-theme geiser use-package helm evil-visual-mark-mode))))
+    (go-mode yaml-mode shackle evil-magit magit exec-path-from-shell markdown-mode helm-ag robe enh-ruby-mode auto-complete evil-smartparens smartparens ag dirtree paredit pastels-on-dark-theme dracula-theme geiser use-package helm evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -172,6 +172,9 @@
   :ensure t)
 
 (use-package yaml-mode
+  :ensure t)
+
+(use-package go-mode
   :ensure t)
 
 (setq geiser-default-implementation 'racket)
