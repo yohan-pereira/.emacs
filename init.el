0afebb12189ead86b109d9341b52c9354639c8e6
Yohan Pereira <yohan.pereira@gmail.com>
2017-10-15 01:19:12 +0530

    fix merge lefover

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
    (quote
     ("f782ed87369a7d568cee28d14922aa6d639f49dd676124d817dd82c8208985d0" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" default)))
  '(package-selected-packages
-   (quote)))
+   (quote
     (evil-surround evil-cleverparens go-mode yaml-mode evil-magit magit exec-path-from-shell markdown-mode helm-ag robe enh-ruby-mode auto-complete smartparens ag dirtree paredit pastels-on-dark-theme dracula-theme geiser use-package helm evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
