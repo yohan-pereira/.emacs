097fa0b4f8eebf47a743ad8d7874edd1ab639a58
Yohan Pereira <yohan.pereira@gmail.com>
2020-02-09 23:27:23 +0530

    added capture template for sdex final rounds

--- init.el
+++ init.el
@@ -17,11 +17,16 @@
  '(custom-safe-themes
    (quote
     ("f782ed87369a7d568cee28d14922aa6d639f49dd676124d817dd82c8208985d0" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" default)))
- '(org-capture-templates
+ `(org-capture-templates
    (quote
-    (("" "" entry
-      (file "~/Documents/notes/todo.org")
-      ""))))
+    (("t" "TODO" entry
+      (file ,(concat org-directory "/todo.org"))
+      "")
+     ("i" "interviews")
+     ("is" "sd[123]")
+     ("isf" "sdx final round" entry
+      (file ,(concat org-directory "/interview-final-sdx.org"))
+      (file ,(concat org-directory "/templates/interviews/sdx-final.org"))))))
  '(package-selected-packages
    (quote
     (evil-org org-plus-contrib cider clojure-mode shackle org-exp org-confluence org evil-surround evil-cleverparens go-mode yaml-mode evil-magit magit exec-path-from-shell markdown-mode helm-ag robe enh-ruby-mode auto-complete smartparens ag dirtree paredit pastels-on-dark-theme dracula-theme geiser use-package helm evil-visual-mark-mode))))
