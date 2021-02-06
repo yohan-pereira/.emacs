b4bdd10d6a335f6644c95f191ad56178990e0b81
Yohan Pereira <yohan.pereira@gmail.com>
2021-02-06 20:26:30 +0530

    staticly configure org capture templates

--- custom.el
+++ custom.el
@@ -4,34 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f782ed87369a7d568cee28d14922aa6d639f49dd676124d817dd82c8208985d0" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" default))
- '(org-capture-templates
-   '(("t" "TODO" entry
-      (file "~/documents/notes-org/todo.org")
-      (file "~/documents/notes-org/templates/todo.org"))
-     ("i" "interviews")
-     ("ie" "EM")
-     ("iet" "EM telephonic" entry
-      (file "~/documents/notes-org/interviews.org")
-      (file "~/documents/notes-org/templates/interviews/em-telephonic.org"))
-     ("ie1" "EM 1" entry
-      (file "~/documents/notes-org/interviews.org")
-      (file "~/documents/notes-org/templates/interviews/em-1.org"))
-     ("ie2" "EM 2" entry
-      (file "~/documents/notes-org/interviews.org")
-      (file "~/documents/notes-org/templates/interviews/em-2.org"))
-     ("is" "sd[123]")
-     ("isf" "sdx em round" entry
-      (file "~/documents/notes-org/interviews.org")
-      (file "~/documents/notes-org/templates/interviews/sdx-em.org"))
-     ("isf" "sdx final round" entry
-      (file "~/documents/notes-org/interviews.org")
-      (file "~/documents/notes-org/templates/interviews/sdx-final.org"))))
- '(org-journal-dir "~/documents/notes-org/journal/" t)
+   (quote
+    ("f782ed87369a7d568cee28d14922aa6d639f49dd676124d817dd82c8208985d0" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" default)))
  '(org-startup-indented t)
  '(package-selected-packages
-   '(elisp-slime-nav rainbow-delimiters evil-org org-plus-contrib cider clojure-mode shackle org-exp org-confluence org evil-surround evil-cleverparens go-mode yaml-mode evil-magit magit exec-path-from-shell markdown-mode helm-ag robe enh-ruby-mode auto-complete smartparens ag dirtree paredit pastels-on-dark-theme dracula-theme geiser use-package helm evil-visual-mark-mode)))
-
+   (quote
+    (evil-org org-plus-contrib cider clojure-mode shackle org-exp org-confluence org evil-surround evil-cleverparens go-mode yaml-mode evil-magit magit exec-path-from-shell markdown-mode helm-ag robe enh-ruby-mode auto-complete smartparens ag dirtree paredit pastels-on-dark-theme dracula-theme geiser use-package helm evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
